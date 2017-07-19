646cf63bf611be341f9416e7af96798f3d417626
amccaf1work <andrew.mccaffrey@nist.gov>
2017-07-19 18:31:54 -0400

    Model development from Java to XML

--- src/main/java/gov/nist/healthcare/cda/model/AutopsyDetails.java
+++ src/main/java/gov/nist/healthcare/cda/model/AutopsyDetails.java
@@ -6,15 +6,26 @@
 package gov.nist.healthcare.cda.model;
 
 import gov.nist.healthcare.cda.model.jdbc.DatabaseConnection;
+import hl7OrgV3.BL;
+import hl7OrgV3.CD;
+import hl7OrgV3.CS;
+import hl7OrgV3.II;
+import hl7OrgV3.POCDMT000040EntryRelationship;
+import hl7OrgV3.POCDMT000040Observation;
+import hl7OrgV3.POCDMT000040Procedure;
+import hl7OrgV3.XActMoodDocumentObservation;
+import hl7OrgV3.XActRelationshipEntryRelationship;
+import hl7OrgV3.XDocumentProcedureMood;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.apache.xmlbeans.XmlOptions;
 
 /**
  *
  * @author mccaffrey
  */
 public class AutopsyDetails {
-    
+
     //private String autopsyDetailsID = null;
     private Boolean resultsAvailable = null;
 
@@ -31,32 +42,91 @@ public class AutopsyDetails {
     public void setResultsAvailable(Boolean resultsAvailable) {
         this.resultsAvailable = resultsAvailable;
     }
-    
-        
+
     public static AutopsyDetails getAutopsyDetailsById(String id) throws SQLException {
         AutopsyDetails ad = new AutopsyDetails();
-        
+
         DatabaseConnection db = new DatabaseConnection();
         StringBuilder sql = new StringBuilder();
         sql.append("SELECT * ");
         sql.append("FROM " + DatabaseConnection.AUTOPSY_DETAILS_NAME + " ");
         sql.append("WHERE " + DatabaseConnection.AUTOPSY_DETAILS_ID + " = '" + id + "';");
-        
+
         ResultSet result = db.executeQuery(sql.toString());
-        
+
         Boolean resultsAvail = null;
-        
-        if(result.next()) {
-            resultsAvail = result.getBoolean(DatabaseConnection.AUTOPSY_DETAILS_RESULTS_AVAILABLE);            
+
+        if (result.next()) {
+            resultsAvail = result.getBoolean(DatabaseConnection.AUTOPSY_DETAILS_RESULTS_AVAILABLE);
         } else {
             return null;
         }
         ad.setResultsAvailable(resultsAvail);
-        
+
         return ad;
     }
 
-    
-    
-    
+    public static POCDMT000040Procedure populateProcedure(POCDMT000040Procedure procedure, AutopsyDetails au) {
+
+        procedure.setClassCode("PROC");
+        procedure.setMoodCode(XDocumentProcedureMood.EVN);
+        if (au.getResultsAvailable()) {
+            procedure.setNegationInd(false);
+        } else {
+            procedure.setNegationInd(true);
+        }
+        II templateId = procedure.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.2");
+
+        CD procedureCode = procedure.addNewCode();
+        procedureCode.setCode("29240004");
+        procedureCode.setDisplayName("Autopsy Procedure");
+        procedureCode.setCodeSystem("2.16.840.1.113883.6.96");
+        procedureCode.setCodeSystemName("SNOMED CT");
+
+        CS statusCode = procedure.addNewStatusCode();
+        if (au.getResultsAvailable()) {
+            statusCode.setCode("completed");
+        }
+
+        procedure.addNewEffectiveTime();
+
+        POCDMT000040EntryRelationship availability = procedure.addNewEntryRelationship();
+        availability.setTypeCode(XActRelationshipEntryRelationship.COMP);
+
+        POCDMT000040Observation observation = availability.addNewObservation();
+        observation.setClassCode("OBS");
+        observation.setMoodCode(XActMoodDocumentObservation.EVN);
+
+        II templateId2 = observation.addNewTemplateId();
+        templateId2.setRoot("2.16.840.1.113883.10.20.26.1.3");
+        templateId2.setExtension("2016-12-01");
+
+        CD availCode = observation.addNewCode();
+        availCode.setCode("69436-4");
+        availCode.setCodeSystem("2.16.840.1.113883.6.1");
+        availCode.setCodeSystemName("LOINC");
+        availCode.setDisplayName("Autopsy results available");
+
+        BL value = BL.Factory.newInstance();
+        value.setValue(true);
+        observation.addNewValue();
+        observation.setValueArray(0, value);
+
+        return procedure;
+    }
+
+    public final static void main(String[] args) throws SQLException {
+
+        AutopsyDetails ad = AutopsyDetails.getAutopsyDetailsById("AD1");
+        POCDMT000040Procedure procedure = POCDMT000040Procedure.Factory.newInstance();
+        AutopsyDetails.populateProcedure(procedure, ad);
+        XmlOptions options = new XmlOptions();
+        options.setCharacterEncoding("UTF-8");
+        options.setSavePrettyPrint();
+
+        System.out.println(procedure.xmlText(options));
+
+    }
+
 }
--- src/main/java/gov/nist/healthcare/cda/model/CauseOfDeath.java
+++ src/main/java/gov/nist/healthcare/cda/model/CauseOfDeath.java
@@ -6,6 +6,23 @@
 package gov.nist.healthcare.cda.model;
 
 import gov.nist.healthcare.cda.model.jdbc.DatabaseConnection;
+import hl7OrgV3.CD;
+import hl7OrgV3.CE;
+import hl7OrgV3.CS;
+import hl7OrgV3.II;
+import hl7OrgV3.INT;
+import hl7OrgV3.POCDMT000040Component4;
+import hl7OrgV3.POCDMT000040Entry;
+import hl7OrgV3.POCDMT000040EntryRelationship;
+import hl7OrgV3.POCDMT000040Observation;
+import hl7OrgV3.POCDMT000040Organizer;
+import hl7OrgV3.POCDMT000040Section;
+import hl7OrgV3.ST;
+import hl7OrgV3.StrucDocText;
+import hl7OrgV3.XActClassDocumentEntryOrganizer;
+import hl7OrgV3.XActMoodDocumentObservation;
+import hl7OrgV3.XActRelationshipEntryRelationship;
+import java.math.BigInteger;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -87,7 +104,6 @@ public class CauseOfDeath {
 
         ResultSet result = db.executeQuery(sql.toString());
 
-        
         if (result.next()) {
             cod.setCauseOfDeath(result.getString(DatabaseConnection.CAUSE_OF_DEATH_CAUSE_OF_DEATH));
             cod.setDiseaseOnsetToDeathInterval(result.getString(DatabaseConnection.CAUSE_OF_DEATH_DISEASE_ONSET_TO_DEATH_INTERVAL));
@@ -95,9 +111,340 @@ public class CauseOfDeath {
             cod.setTobaccoUse(result.getString(DatabaseConnection.CAUSE_OF_DEATH_TOBACCO_USE));
         } else {
             return null;
-        }        
+        }
 
         return cod;
     }
+
+    public static POCDMT000040Section populateCauseOfDeathSection(POCDMT000040Section section, CauseOfDeath cod) {
+
+        II templateIdSection = section.addNewTemplateId();
+        templateIdSection.setRoot("2.16.840.1.113883.10.20.26.1.2.5");
+        templateIdSection.setExtension("2016-12-01");
+
+        CE code = section.addNewCode();
+        code.setCode("11450-4");
+        code.setCodeSystem("2.16.840.1.113883.6.1");
+        code.setCodeSystemName("LOINC");
+        code.setDisplayName("Problem list");
+
+        ST title = section.addNewTitle();
+        title.newCursor().setTextValue("Coded Cause Of Death Section");
+
+        StrucDocText text = section.addNewText();
+        text.newCursor().setTextValue("Placeholder");
+
+        POCDMT000040Entry causalInformationEntry = section.addNewEntry();
+        POCDMT000040Organizer causalInformationOrganizer = causalInformationEntry.addNewOrganizer();
+        CauseOfDeath.populateCausalInformationOrganizer(causalInformationOrganizer, cod);
+
+        POCDMT000040Entry injuryEntry = section.addNewEntry();
+        POCDMT000040Organizer injuryOrganizer = injuryEntry.addNewOrganizer();
+        CauseOfDeath.populateInjuryOrganizer(injuryOrganizer, cod);
+
+        POCDMT000040Entry pregnancyStatusEntry = section.addNewEntry();
+        POCDMT000040Observation pregnancyStatusObservation = pregnancyStatusEntry.addNewObservation();
+        CauseOfDeath.populatePregnancyStatusObservation(pregnancyStatusObservation, cod);
+
+        POCDMT000040Entry tobaccoUseEntry = section.addNewEntry();
+        POCDMT000040Observation tobaccoUseObservation = tobaccoUseEntry.addNewObservation();
+        CauseOfDeath.populateTobaccoUseObservation(tobaccoUseObservation, cod);
+
+        POCDMT000040Entry activityEntry = section.addNewEntry();
+        POCDMT000040Observation activityObservation = activityEntry.addNewObservation();
+        CauseOfDeath.populateActivityAtTimeOfDeathObservation(activityObservation, cod);
+
+        return section;
+        
+    }
+
+    private static POCDMT000040Organizer populateCausalInformationOrganizer(POCDMT000040Organizer organizer, CauseOfDeath cod) {
+
+        organizer.setClassCode(XActClassDocumentEntryOrganizer.CLUSTER);
+        organizer.setMoodCode("EVN");
+
+        II templateId = organizer.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.3.26");
+        templateId.setExtension("2016-12-01");
+
+        CS statusCode = organizer.addNewStatusCode();
+        statusCode.setCode("completed");
+
+        POCDMT000040Component4 entityAxisCodeComponent = organizer.addNewComponent();
+        entityAxisCodeComponent.addNewSequenceNumber().setValue(BigInteger.ONE);
+        entityAxisCodeComponent.addNewSeperatableInd().setValue(false);
+
+        POCDMT000040Observation axisCodeObservation = entityAxisCodeComponent.addNewObservation();
+        CauseOfDeath.populateEntityAxisCodeObservation(axisCodeObservation, cod);
+
+        POCDMT000040Component4 recordAxisCodeComponent = organizer.addNewComponent();
+        POCDMT000040Observation recordAxisCodeObservation = recordAxisCodeComponent.addNewObservation();
+        CauseOfDeath.populateRecordAxisCodeObservation(recordAxisCodeObservation, cod);
+
+        POCDMT000040Component4 underlyingComponent = organizer.addNewComponent();
+        POCDMT000040Observation underlyingObservation = underlyingComponent.addNewObservation();
+        CauseOfDeath.populateUnderlyingObservation(underlyingObservation, cod);
+
+        POCDMT000040Component4 conversionFlagComponent = organizer.addNewComponent();
+        POCDMT000040Observation conversionFlagObservation = conversionFlagComponent.addNewObservation();
+        CauseOfDeath.populateConversionFlagObservation(conversionFlagObservation, cod);
+
+        return organizer;
+        
+    }
+
+    private static POCDMT000040Observation populateEntityAxisCodeObservation(POCDMT000040Observation observation, CauseOfDeath cod) {
+        observation.setClassCode("OBS");
+        observation.setMoodCode(XActMoodDocumentObservation.EVN);
+
+        II templateId = observation.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.3.27");
+        templateId.setExtension("2016-12-01");
+
+        CD code = observation.addNewCode();
+        code.setCode("80356-9");
+        code.setCodeSystem("2.16.840.1.113883.6.1");
+        code.setCodeSystemName("LOINC");
+        code.setDisplayName("Cause of death entity axis code");
+
+        CD value = CD.Factory.newInstance();
+        value.setCode("ICD-10 GOES HERE");
+        observation.addNewValue();
+        observation.setValueArray(0, value);
+
+        POCDMT000040EntryRelationship relationship = observation.addNewEntryRelationship();
+        relationship.setTypeCode(XActRelationshipEntryRelationship.COMP);
+        POCDMT000040Observation sequenceWithinLineObservation = relationship.addNewObservation();
+        CauseOfDeath.populateSequenceWithinLineObservation(sequenceWithinLineObservation, cod);
+
+        return observation;
+    }
+
+    private static POCDMT000040Observation populateSequenceWithinLineObservation(POCDMT000040Observation observation, CauseOfDeath cod) {
+
+        observation.setClassCode("OBS");
+        observation.setMoodCode(XActMoodDocumentObservation.EVN);
+
+        II templateId = observation.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.3.30");
+        templateId.setExtension("2016-12-01");
+
+        CD code = observation.addNewCode();
+        code.setCode("PHC1427");
+        code.setCodeSystem("2.16.840.1.114222.4.5.274");
+        code.setCodeSystemName("PHIN VS (CDC Local Coding System)");
+        code.setDisplayName("Sequence within line");
+
+        INT value = INT.Factory.newInstance();
+        value.setValue(BigInteger.ONE);
+        observation.addNewValue();
+        observation.setValueArray(0, value);
+
+        return observation;
+    }
+
+    private static POCDMT000040Observation populateRecordAxisCodeObservation(POCDMT000040Observation observation, CauseOfDeath cod) {
+
+        observation.setClassCode("OBS");
+        observation.setMoodCode(XActMoodDocumentObservation.EVN);
+
+        II templateId = observation.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.3.28");
+        templateId.setExtension("2016-12-01");
+
+        CD code = observation.addNewCode();
+        code.setCode("80357-7");
+        code.setCodeSystem("2.16.840.1.113883.6.1");
+        code.setCodeSystemName("LOINC");
+        code.setDisplayName("Cause of death record axis code");
+
+        CD value = CD.Factory.newInstance();
+        value.setCode("ICD-10 GOES HERE");
+        observation.addNewValue();
+        observation.setValueArray(0, value);
+
+        return observation;
+
+    }
+
+    private static POCDMT000040Observation populateUnderlyingObservation(POCDMT000040Observation observation, CauseOfDeath cod) {
+
+        observation.setClassCode("OBS");
+        observation.setMoodCode(XActMoodDocumentObservation.EVN);
+
+        II templateId = observation.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.3.29");
+        templateId.setExtension("2016-12-01");
+
+        CD code = observation.addNewCode();
+        code.setCode("80358-5");
+        code.setCodeSystem("2.16.840.1.113883.6.1");
+        code.setCodeSystemName("LOINC");
+        code.setDisplayName("Cause of death.underlying [Automated]");
+
+        CD value = CD.Factory.newInstance();
+        value.setCode("ICD-10 GOES HERE");
+        observation.addNewValue();
+        observation.setValueArray(0, value);
+
+        return observation;
+
+    }
+
+    private static POCDMT000040Observation populateConversionFlagObservation(POCDMT000040Observation observation, CauseOfDeath cod) {
+
+        observation.setClassCode("OBS");
+        observation.setMoodCode(XActMoodDocumentObservation.EVN);
+
+        II templateId = observation.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.3.31");
+        templateId.setExtension("2016-12-01");
+
+        CD code = observation.addNewCode();
+        code.setCode("PHC1422");
+        code.setCodeSystem("2.16.840.1.114222.4.5.274");
+        code.setCodeSystemName("PHIN VS (CDC Local Coding System)");
+        code.setDisplayName("Conversion Flag");
+
+        CD value = CD.Factory.newInstance();
+        value.setCode("PHC1376");
+        value.setCodeSystem("2.16.840.1.114222.4.5.274");
+        value.setCodeSystemName("PHIN VS (CDC Local Coding System)");
+        value.setDisplayName("Conversion using non-ambivalent table entries");
+        observation.addNewValue();
+        observation.setValueArray(0, value);
+
+        return observation;
+        
+    }
+
+    private static POCDMT000040Organizer populateInjuryOrganizer(POCDMT000040Organizer organizer, CauseOfDeath cod) {
+        
+        organizer.setClassCode(XActClassDocumentEntryOrganizer.CLUSTER);
+        organizer.setMoodCode("EVN");
+
+        II templateId = organizer.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.9");
+        templateId.setExtension("2016-12-01");
+
+        CS statusCode = organizer.addNewStatusCode();
+        //statusCode.setCode("completed");
+
+        POCDMT000040Component4 injuryComponent = organizer.addNewComponent();
+        POCDMT000040Observation injuryInvolvedInDeathObservation = injuryComponent.addNewObservation();
+        CauseOfDeath.populateInjuryInvolvedInDeathObservation(injuryInvolvedInDeathObservation,cod);
+        
+        return organizer;
+        
+    }
+
+    private static POCDMT000040Observation populateInjuryInvolvedInDeathObservation(POCDMT000040Observation observation, CauseOfDeath cod) {
+        observation.setClassCode("OBS");
+        observation.setMoodCode(XActMoodDocumentObservation.EVN);
+
+        II templateId = observation.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.3.40");
+        templateId.setExtension("2016-12-01");
+
+        CD code = observation.addNewCode();
+        code.setCode("71481-6");
+        code.setCodeSystem("2.16.840.1.113883.6.1");
+        code.setCodeSystemName("LOINC");
+        code.setDisplayName("Did the death of this person involve injury of any kind");
+        
+        CD value = CD.Factory.newInstance();
+        value.setCode(cod.getInjuryInvolvedInDeath());
+        observation.addNewValue();
+        observation.setValueArray(0, value);
+        
+        return observation;
+        
+        
+    }
+
+    private static POCDMT000040Observation populatePregnancyStatusObservation(POCDMT000040Observation observation, CauseOfDeath cod) {
+        observation.setClassCode("OBS");
+        observation.setMoodCode(XActMoodDocumentObservation.EVN);
+
+        II templateId = observation.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.1.2");
+        templateId.setExtension("2016-12-01");
+
+        CD code = observation.addNewCode();
+        code.setCode("69442-2");
+        code.setCodeSystem("2.16.840.1.113883.6.1");
+        code.setCodeSystemName("LOINC");
+        code.setDisplayName("Pregnancy Status");
+        
+
+        //TODO: Hardcoded for now
+        CD value = CD.Factory.newInstance();
+        value.setCode("PHC1260");
+        value.setCodeSystem("2.16.840.1.114222.4.5.274");
+        value.setCodeSystemName("PHIN VS (CDC Local Coding System)");
+        value.setDisplayName("Not pregnant within the past year");
+        observation.addNewValue();
+        observation.setValueArray(0, value);
+        
+        return observation;
+        
+    }
+
+    private static POCDMT000040Observation populateTobaccoUseObservation(POCDMT000040Observation observation, CauseOfDeath cod) {
+        
+        observation.setClassCode("OBS");
+        observation.setMoodCode(XActMoodDocumentObservation.EVN);
+
+        II templateId = observation.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.1.1");
+        templateId.setExtension("2016-12-01");
+
+        CD code = observation.addNewCode();
+        code.setCode("69443-0");
+        code.setCodeSystem("2.16.840.1.113883.6.1");
+        code.setCodeSystemName("LOINC");
+        code.setDisplayName("Did tobacco use contribute to death");
+        
+
+        CD value = CD.Factory.newInstance();
+        value.setCode(cod.tobaccoUse);
+        value.setCodeSystem("2.16.840.1.113883.6.96");
+        value.setCodeSystemName("SNOMED CT");
+       
+        observation.addNewValue();
+        observation.setValueArray(0, value);
+                
+        return observation;
+        
+    }
+    private static POCDMT000040Observation populateActivityAtTimeOfDeathObservation(POCDMT000040Observation observation, CauseOfDeath cod) {
+        
+        observation.setClassCode("OBS");
+        observation.setMoodCode(XActMoodDocumentObservation.EVN);
+
+        II templateId = observation.addNewTemplateId();
+        templateId.setRoot("2.16.840.1.113883.10.20.26.1.3.21");
+        templateId.setExtension("2016-12-01");
+
+        CD code = observation.addNewCode();
+        code.setCode("80626-5");
+        code.setCodeSystem("2.16.840.1.113883.6.1");
+        code.setCodeSystemName("LOINC");
+        code.setDisplayName("Activity at time of death");
+        
+        //Hardcoded for now
+        CD value = CD.Factory.newInstance();
+        value.setCode("PHC1350");
+        code.setCodeSystem("2.16.840.1.114222.4.5.274");
+        code.setCodeSystemName("PHIN VS (CDC Local Coding System)");
+        code.setDisplayName("During unspecified activity");
+        
+        observation.addNewValue();
+        observation.setValueArray(0, value);
+                
+        return observation;
+    }
+
+    
 
 }
